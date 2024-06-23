5b97f63ccf6cb77d8faa225f8d950abe8bf84acc
Admin <maayush235@gmail.com>
2024-06-23 23:38:38 +0530

    Radhey Radhey

--- lib/Views/HomeScreen/home_Screen.dart
+++ lib/Views/HomeScreen/home_Screen.dart
@@ -17,51 +17,88 @@ class DohaApp extends StatelessWidget {
             decoration: BoxDecoration(
               gradient: LinearGradient(
                 colors: themeProvider.isDarkMode
-                    ? [Colors.black, Colors.grey]
+                    ? [Colors.black, Colors.grey.shade800]
                     : [Colors.white, Colors.blue],
                 begin: Alignment.topLeft,
                 end: Alignment.bottomRight,
               ),
             ),
             child: Column(
-
               children: [
-               const Spacer(), // Space for the icons at the top
+                // Space for the icons at the top
 
+                // Removed Expanded
                 Expanded(
                   child: ListView.builder(
+                    shrinkWrap: true,
                     itemCount: languageProvider.dohaData.length,
                     itemBuilder: (context, index) {
-                      return ListTile(
-                        title: Column(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Text(
-                              languageProvider.selectedLanguage == 'Hindi'
-                                  ? languageProvider.dohaData[index].hindi
-                                  : languageProvider.selectedLanguage == 'English'
-                                  ? languageProvider.dohaData[index].english
-                                  : languageProvider.dohaData[index].gujarati,
-                            ),
-                            SizedBox(height: 8.0),
-                            Text(
-                              languageProvider.selectedLanguage == 'Hindi'
-                                  ? languageProvider.dohaData[index].meaningHindi
-                                  : languageProvider.selectedLanguage == 'English'
-                                  ? languageProvider.dohaData[index].meaningEnglish
-                                  : languageProvider.dohaData[index].meaningGujarati,
-                              style: TextStyle(
-                                fontStyle: FontStyle.italic,
-                                color: themeProvider.isDarkMode ? Colors.grey[300] : Colors.grey[700],
-                              ),
+                      return Container(
+                        margin: const EdgeInsets.symmetric(
+                            vertical: 10.0, horizontal: 20.0),
+                        decoration: BoxDecoration(
+                          color: themeProvider.isDarkMode
+                              ? Colors.grey.shade700.withOpacity(0.2)
+                              : Colors.white,
+                          borderRadius: BorderRadius.circular(12),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black.withOpacity(0.2),
+                              spreadRadius: 2,
+                              blurRadius: 7,
+                              offset: Offset(0, 3),
                             ),
                           ],
+                        ),
+                        child: ListTile(
+                          contentPadding: const EdgeInsets.all(16.0),
+                          title: Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Container(
+                                height: 100,
+                                width: 200,
+                                decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                        image:
+                                            AssetImage("assets/newimgs.jpeg"),fit: BoxFit.cover)),
+                              ),
+                              SizedBox(height: 10.0),
+                              Text(
+                                languageProvider.selectedLanguage == 'Hindi'
+                                    ? languageProvider.dohaData[index].hindi
+                                    : languageProvider.selectedLanguage ==
+                                            'English'
+                                        ? languageProvider
+                                            .dohaData[index].english
+                                        : languageProvider
+                                            .dohaData[index].gujarati,
+                              ),
+                              SizedBox(height: 8.0),
+                              Text(
+                                languageProvider.selectedLanguage == 'Hindi'
+                                    ? languageProvider
+                                        .dohaData[index].meaningHindi
+                                    : languageProvider.selectedLanguage ==
+                                            'English'
+                                        ? languageProvider
+                                            .dohaData[index].meaningEnglish
+                                        : languageProvider
+                                            .dohaData[index].meaningGujarati,
+                                style: TextStyle(
+                                  fontStyle: FontStyle.italic,
+                                  color: themeProvider.isDarkMode
+                                      ? Colors.grey[400]
+                                      : Colors.grey[700],
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
                       );
                     },
                   ),
                 ),
-                const Spacer()
               ],
             ),
           ),
@@ -69,7 +106,9 @@ class DohaApp extends StatelessWidget {
             top: 40,
             right: 20,
             child: IconButton(
-              icon: Icon(themeProvider.isDarkMode ? Icons.light_mode : Icons.dark_mode),
+              icon: Icon(themeProvider.isDarkMode
+                  ? Icons.light_mode
+                  : Icons.dark_mode),
               color: Colors.white,
               onPressed: () {
                 themeProvider.toggleTheme();
@@ -80,7 +119,8 @@ class DohaApp extends StatelessWidget {
             top: 40,
             left: 20,
             child: DropdownButton<String>(
-              dropdownColor: themeProvider.isDarkMode ? Colors.grey[900] : Colors.white,
+              dropdownColor:
+                  themeProvider.isDarkMode ? Colors.grey[900] : Colors.white,
               value: languageProvider.selectedLanguage,
               onChanged: (String? newValue) {
                 if (newValue != null) {
@@ -91,7 +131,11 @@ class DohaApp extends StatelessWidget {
                   .map<DropdownMenuItem<String>>((String value) {
                 return DropdownMenuItem<String>(
                   value: value,
-                  child: Text(value, style: TextStyle(color: themeProvider.isDarkMode ? Colors.white : Colors.black)),
+                  child: Text(value,
+                      style: TextStyle(
+                          color: themeProvider.isDarkMode
+                              ? Colors.white
+                              : Colors.black)),
                 );
               }).toList(),
             ),
